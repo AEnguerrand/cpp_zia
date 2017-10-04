01b20f01b143ca540b16712dc123b838e23f18f5
Enguerrand <engue0774@gmail.com>
2017-10-04 17:15:33 +0200

    Master(feat):Base test for travis CI

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+** main.cpp for CPP_ZIA in /home/enguerrand/delivery/CPP_ZIA/main.cpp
+**
+** Made by Enguerrand Allamel
+** Login   <enguerrand.allamel@epitech.eu>
+**
+** Started on  Wed Oct 04 17:01:02 2017 Enguerrand Allamel
+** Last update Wed Oct 04 17:01:02 2017 Enguerrand Allamel
+*/
+
+#include <iostream>
+
+int main(int, char **) {
+  std::cout << "Hello word" << std::endl;
+  return (0);
+}
