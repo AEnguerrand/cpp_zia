d6983f64711c1a99164fd244b4cfd18ae02e9e83
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2018-02-13 16:03:34 +0100

    HttpParser(feature): transform raw request to duplex

---
+++ src/HttpParser/HttpParser.hh
@@ -0,0 +1,39 @@
+//
+// Made by Marco
+//
+
+#ifndef HTTP_PARSER_HPP_
+# define HTTP_PARSER_HPP_
+
+#include <iostream>
+#include <vector>
+#include "HttpParserException.hpp"
+#include "Transform.hpp"
+#include "http.h"
+
+#define CRLF "\r\n"
+#define SP " "
+
+namespace nz
+{
+	class HttpParser
+	{
+	public:
+		HttpParser();
+		virtual ~HttpParser();
+
+	private:
+		zia::api::HttpRequest	GetRequest(const std::vector<std::string> & input);
+		zia::api::HttpResponse	GetResponse(const std::vector<std::string> & input);
+
+	private:
+		zia::api::http::Method	GetMethodFromString(const std::string & input);
+		zia::api::http::Version GetVersionFromString(const std::string & input);
+
+	public:
+		zia::api::HttpDuplex	Parse(zia::api::Net::Raw request);
+		void					CheckValidity(zia::api::HttpRequest);
+	};
+}
+
+#endif //HTTP_PARSER_HPP_
---
+++ src/HttpParser/Transform.hpp
@@ -0,0 +1,76 @@
+//
+// Made by Marco
+//
+
+#ifndef TRANSFORM_HPP_
+# define TRANSFORM_HPP_
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "net.h"
+
+namespace transform
+{
+	static zia::api::Net::Raw StringToRaw(std::string input)
+	{
+		zia::api::Net::Raw output;
+		std::transform(input.begin(), input.end(), std::back_inserter(output),
+			[](unsigned char c) -> std::byte { return static_cast<std::byte>(c); });
+		return output;
+	}
+
+	static std::string RawToString(zia::api::Net::Raw input)
+	{
+		std::string output(input.begin(), input.end());
+		return output;
+	}
+
+	static std::vector<std::string>	Split(const std::string & input, const std::string & delimiter)
+	{
+		std::vector<std::string> output;
+		std::string tmp = input;
+		std::string token;
+		size_t pos = 0;
+
+		while ((pos = tmp.find("\n")) != std::string::npos)
+		{
+			token = tmp.substr(0, pos);
+			output.push_back(token);
+			tmp.erase(0, pos + delimiter.length());
+		}
+		return output;
+	}
+
+	static std::string	EpurStr(const std::string & input)
+	{
+		std::string tmp;
+		std::string output;
+
+		auto data = Split(input, "\n");
+		for (auto it : data)
+		{
+			std::istringstream iss(it);
+			if (iss >> tmp)
+				output += tmp;
+			while (iss >> tmp)
+				output += ' ' + tmp;
+			output += "\n";
+		}
+		return output;
+	}
+
+	static std::string	VectorToString(const std::vector<std::string> & input)
+	{
+		std::string output;
+
+		for (auto it : input)
+			output += it + ' ';
+
+		return output.substr(0, output.size() - 1);
+	}
+}
+
+#endif // TRANSFORM_HPP_
