62a2c15e9cbef73568872197f74fc2dc158206b0
JSX <jean-sebastien.herbaux@epitech.eu>
2018-02-25 01:32:07 +0100

    cppcheck(refactor): Refactor process.cpp

--- src/Process/Process.cpp
+++ src/Process/Process.cpp
@@ -14,7 +14,7 @@ void nz::Process::startProcess(zia::api::HttpDuplex & duplex)
 {
   auto Modules = this->_moduleLoader.getModules();
 
-  for (auto it : Modules) {
+  for (auto& it : Modules) {
       it.second->exec(duplex);
     }
 
